882269a8e1bd25c043d5157ac0e61485e5dc285c
Brandon Usher <brandonu@aeries.com>
2021-04-09 16:04:55 -0700

    Implement College Tests Endpoint

---
+++ AeriesApi/Student/Get-AeriesStudentCollegeTest.ps1
@@ -0,0 +1,43 @@
+function Get-AeriesStudentCollegeTest
+{
+    <#  
+        .SYNOPSIS
+        Get Tests from Aeries
+        
+        .DESCRIPTION
+        The Get-AeriesStudentCollegeTest cmdlet is used to retrieve College Tests for a specific Student from Aeries
+
+        .EXAMPLE
+        Get-AeriesStudentCollegeTest -StudentID 99400001
+        This will return all College Tests for the specific Student ID from Aeries
+    #>
+
+    [CmdletBinding()]
+    param (
+        [ValidateRange(1, [Int16]::MaxValue)]
+        [Parameter(Mandatory=$true)]
+        [Int16]
+        $SchoolCode,
+        [ValidateRange(1, [Int]::MaxValue)]
+        [Parameter(Mandatory=$false)]
+        [int]
+        $StudentID
+    )
+
+    Begin {
+        Write-Verbose -Message "Begin running $($MyInvocation.MyCommand)"
+
+        $Method = "Get"
+        $SuccessStatusCode = 200
+        $Endpoint = "v5/schools/$SchoolCode/CollegeTestScores/$StudentID"
+    }
+
+    Process {
+        $Result = (Invoke-AeriesApiCall -Method $Method -Endpoint $Endpoint -SuccessStatusCode $SuccessStatusCode)
+    }
+
+    End {
+        Write-Verbose -Message "Finish running $($MyInvocation.MyCommand)"
+        return $Result
+    }
+}
