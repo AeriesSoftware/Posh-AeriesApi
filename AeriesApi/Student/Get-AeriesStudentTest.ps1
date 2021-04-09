2dec7b0d8056c936318cb00db9bd01eb7325e9c8
Brandon Usher <brandonu@aeries.com>
2021-04-09 15:51:39 -0700

    Implement Test Scores endpoint

---
+++ AeriesApi/Student/Get-AeriesStudentTest.ps1
@@ -0,0 +1,39 @@
+function Get-AeriesStudentTest
+{
+    <#  
+        .SYNOPSIS
+        Get Tests from Aeries
+        
+        .DESCRIPTION
+        The Get-AeriesStudentTest cmdlet is used to retrieve Tests for a specific Student from Aeries
+
+        .EXAMPLE
+        Get-AeriesStudentTest -StudentID 99400001
+        This will return all Tests for the specific Student ID from Aeries
+    #>
+
+    [CmdletBinding()]
+    param (
+        [ValidateRange(1, [Int]::MaxValue)]
+        [Parameter(Mandatory=$true)]
+        [int]
+        $StudentID
+    )
+
+    Begin {
+        Write-Verbose -Message "Begin running $($MyInvocation.MyCommand)"
+
+        $Method = "Get"
+        $SuccessStatusCode = 200
+        $Endpoint = "v5/students/$StudentID/tests"
+    }
+
+    Process {
+        $Result = (Invoke-AeriesApiCall -Method $Method -Endpoint $Endpoint -SuccessStatusCode $SuccessStatusCode)
+    }
+
+    End {
+        Write-Verbose -Message "Finish running $($MyInvocation.MyCommand)"
+        return $Result
+    }
+}
