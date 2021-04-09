88e661d5577873e6338e74a83fe2d2630f8f8b08
Brandon Usher <brandonu@aeries.com>
2021-04-09 16:19:09 -0700

    Implement Fees and Fines

---
+++ AeriesApi/Student/Get-AeriesFee.ps1
@@ -0,0 +1,47 @@
+function Get-AeriesFee
+{
+    <#  
+        .SYNOPSIS
+        Get Fees and Fines from Aeries
+        
+        .DESCRIPTION
+        The Get-AeriesFee cmdlet is used to retrieve Fees and Fines for all or a specific Student from Aeries
+
+        .EXAMPLE
+        Get-AeriesFee -SchoolCode 994
+        This will return all Fees and Fines for all Students at the specified School Code from Aeries
+
+        .EXAMPLE
+        Get-AeriesFee -SchoolCode 994 -StudentID 99400001
+        This will return all Fees and Fines for the specific Student ID at the specified School Code from Aeries
+    #>
+
+    [CmdletBinding()]
+    param (
+        [ValidateRange(1, [Int16]::MaxValue)]
+        [Parameter(Mandatory=$true)]
+        [Int16]
+        $SchoolCode,
+        [ValidateRange(1, [Int]::MaxValue)]
+        [Parameter(Mandatory=$false)]
+        [int]
+        $StudentID
+    )
+
+    Begin {
+        Write-Verbose -Message "Begin running $($MyInvocation.MyCommand)"
+
+        $Method = "Get"
+        $SuccessStatusCode = 200
+        $Endpoint = "v5/schools/$SchoolCode/fees/$StudentID"
+    }
+
+    Process {
+        $Result = (Invoke-AeriesApiCall -Method $Method -Endpoint $Endpoint -SuccessStatusCode $SuccessStatusCode)
+    }
+
+    End {
+        Write-Verbose -Message "Finish running $($MyInvocation.MyCommand)"
+        return $Result
+    }
+}
