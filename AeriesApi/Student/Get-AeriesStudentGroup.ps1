aa46d63b4efc4c63f3f70677e237399479b3357d
Brandon Usher <brandonu@aeries.com>
2021-04-09 17:07:01 -0700

    Implement Student Groups

---
+++ AeriesApi/Student/Get-AeriesStudentGroup.ps1
@@ -0,0 +1,39 @@
+function Get-AeriesStudentGroup
+{
+    <#  
+        .SYNOPSIS
+        Get Student Groups from Aeries
+        
+        .DESCRIPTION
+        The Get-AeriesStudentGroup cmdlet is used to retrieve Student Groups for all Students from Aeries
+
+        .EXAMPLE
+        Get-AeriesStudentGroup -SchoolCode 994
+        This will return all Student Groups for all Students at the specified School Code from Aeries
+    #>
+
+    [CmdletBinding()]
+    param (
+        [ValidateRange(1, [Int16]::MaxValue)]
+        [Parameter(Mandatory=$true)]
+        [Int16]
+        $SchoolCode
+    )
+
+    Begin {
+        Write-Verbose -Message "Begin running $($MyInvocation.MyCommand)"
+
+        $Method = "Get"
+        $SuccessStatusCode = 200
+        $Endpoint = "v5/schools/$SchoolCode/StudentGroups"
+    }
+
+    Process {
+        $Result = (Invoke-AeriesApiCall -Method $Method -Endpoint $Endpoint -SuccessStatusCode $SuccessStatusCode)
+    }
+
+    End {
+        Write-Verbose -Message "Finish running $($MyInvocation.MyCommand)"
+        return $Result
+    }
+}
