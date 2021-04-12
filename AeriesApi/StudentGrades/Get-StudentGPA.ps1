bc5c14d3a1100d9e1a164ef33c1b8bbf44a47b26
Brandon Usher <brandonu@aeries.com>
2021-04-12 13:50:04 -0700

    Implement Student GPA GET endpoint

---
+++ AeriesApi/StudentGrades/Get-StudentGPA.ps1
@@ -0,0 +1,67 @@
+function Get-AeriesStudentGPA
+{
+    <#  
+        .SYNOPSIS
+        Get one or more Students GPAs from Aeries
+        
+        .DESCRIPTION
+        The Get-AeriesStudentGPA cmdlet is used to retrieve Programs for all or a specific Student from Aeries
+
+        .EXAMPLE
+        Get-AeriesStudentGPA -SchoolCode 994
+        This will return all Student GPAs from Aeries at School 994
+
+        .EXAMPLE
+        Get-AeriesStudentGPA -SchoolCode 994 -StudentID 99400001
+        This will return the GPAs for School 994 and Student ID 99400001
+    #>
+
+    [CmdletBinding()]
+    param (
+        [ValidateRange(1, [Int16]::MaxValue)]
+        [Parameter(Mandatory=$true)]
+        [Int16]
+        $SchoolCode,
+        [ValidateRange(1, [Int]::MaxValue)]
+        [Parameter(Mandatory=$false)]
+        [int]
+        $StudentID,
+        # The Record-Count value of the first record to return (inclusive)
+        [Parameter(Mandatory=$false)]
+        [int]
+        $StartingRecord,
+        # The Record-Count value of the last record to return (inclusive)
+        [Parameter(Mandatory=$false)]
+        [int]
+        $EndingRecord
+    )
+
+    Begin {
+        Write-Verbose -Message "Begin running $($MyInvocation.MyCommand)"
+
+        $Method = "Get"
+        $SuccessStatusCode = 200
+        $Endpoint = "v5/schools/$SchoolCode/gpas/$StudentId"
+
+        $QueryParameters = @{}
+        if ($StartingRecord -ge 1) {
+            $QueryParameters += @{
+                "StartingRecord" = $StartingRecord
+            }
+        }
+        if ($EndingRecord -ge 1) {
+            $QueryParameters += @{
+                "EndingRecord" = $EndingRecord
+            }
+        }
+    }
+
+    Process {
+        $Result = (Invoke-AeriesApiCall -Method $Method -Endpoint $Endpoint -SuccessStatusCode $SuccessStatusCode -QueryParameters $QueryParameters)
+    }
+
+    End {
+        Write-Verbose -Message "Finish running $($MyInvocation.MyCommand)"
+        return $Result
+    }
+}
