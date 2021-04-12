c94ac2b61680f84eb679536879a334d20f2113bb
Brandon Usher <brandonu@aeries.com>
2021-04-12 16:57:47 -0700

    Implemented Student Enrollment History GET endpoint

---
+++ AeriesApi/AttendanceEnrollment/Get-AeriesStudentEnrollmentHistory.ps1
@@ -0,0 +1,69 @@
+function Get-AeriesStudentEnrollmentHistory
+{
+    <#  
+        .SYNOPSIS
+        Get Enrollment History from Aeries
+        
+        .DESCRIPTION
+        The Get-AeriesStudentEnrollmentHistory cmdlet is used to retrieve a specific or all of Student Enrollment Histories from Aeries
+
+        .EXAMPLE
+        Get-AeriesStudentEnrollmentHistory
+        This will return all Student Enrollment Histories from Aeries
+
+        .EXAMPLE
+        Get-AeriesStudentEnrollmentHistory -SchoolCode 994
+        This will return the requested something from Aeries for School Code 994
+    #>
+
+    [CmdletBinding()]
+    param (
+        [ValidateRange(1, [Int16]::MaxValue)]
+        [Parameter(Mandatory=$false)]
+        [Int16]
+        $SchoolCode,
+        [ValidateRange(1, [Int]::MaxValue)]
+        [Parameter(Mandatory=$false)]
+        [int]
+        $StudentID,
+        # Academic year (2017 for 2017-2018)
+        [ValidateLength(4,4)]
+        [Parameter(Mandatory=$false)]
+        [string]
+        $AcademicYear
+    )
+
+    Begin {
+        Write-Verbose -Message "Begin running $($MyInvocation.MyCommand)"
+
+        $Method = "Get"
+        $SuccessStatusCode = 200
+        $Endpoint = "v5"
+
+        if (($SchoolCode -gt 0) -and (![string]::IsNullOrWhiteSpace($AcademicYear))) {
+            # School Code and Academic Year were provided
+            $Endpoint += "/schools/$SchoolCode/enrollment/$StudentID/year/$AcademicYear"
+        }
+        elseif ($SchoolCode -gt 0) {
+            # School Code is greater than 0
+            $Endpoint += "/schools/$SchoolCode/enrollment/$StudentID"
+        }
+        elseif (![string]::IsNullOrWhiteSpace($AcademicYear)) {
+            # Academic Year was provided
+            $Endpoint += "/enrollment/$StudentID/year/$AcademicYear"
+        }
+        else {
+            # Default (can return a LOT of data if no Student ID is passed)
+            $Endpoint += "/enrollment/$StudentID"
+        }
+    }
+
+    Process {
+        $Result = (Invoke-AeriesApiCall -Method $Method -Endpoint $Endpoint -SuccessStatusCode $SuccessStatusCode)
+    }
+
+    End {
+        Write-Verbose -Message "Finish running $($MyInvocation.MyCommand)"
+        return $Result
+    }
+}
