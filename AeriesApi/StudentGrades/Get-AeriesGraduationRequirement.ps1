ca4753c5e0af9e76e2e15da2f38355b432c1a432
Brandon Usher <brandonu@aeries.com>
2021-04-12 14:18:56 -0700

    Implement Graduation Requirements GET endpoint

---
+++ AeriesApi/StudentGrades/Get-AeriesGraduationRequirement.ps1
@@ -0,0 +1,39 @@
+function Get-AeriesGraduationRequirement
+{
+    <#  
+        .SYNOPSIS
+        Get Graduation Requirements from Aeries
+        
+        .DESCRIPTION
+        The Get-AeriesGraduationRequirement cmdlet is used to retrieve Graduation Requirements for a specific School from Aeries
+
+        .EXAMPLE
+        Get-AeriesGraduationRequirement -SchoolCode 994
+        This will return Graduation Requirements from Aeries at School 994
+    #>
+
+    [CmdletBinding()]
+    param (
+        [ValidateRange(1, [Int16]::MaxValue)]
+        [Parameter(Mandatory=$true)]
+        [Int16]
+        $SchoolCode
+    )
+
+    Begin {
+        Write-Verbose -Message "Begin running $($MyInvocation.MyCommand)"
+
+        $Method = "Get"
+        $SuccessStatusCode = 200
+        $Endpoint = "v5/schools/$SchoolCode/GraduationRequirements"
+    }
+
+    Process {
+        $Result = (Invoke-AeriesApiCall -Method $Method -Endpoint $Endpoint -SuccessStatusCode $SuccessStatusCode)
+    }
+
+    End {
+        Write-Verbose -Message "Finish running $($MyInvocation.MyCommand)"
+        return $Result
+    }
+}
