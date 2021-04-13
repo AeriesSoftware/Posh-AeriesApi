8718d2ddd264e4badafd0a0beffc40119dfdf6ea
Brandon Usher <brandonu@aeries.com>
2021-04-13 13:58:58 -0700

    Update to include Staff-Teacher GET endpoint

--- AeriesApi/Methods/Staff/Get-AeriesTeacher.ps1
+++ AeriesApi/Methods/Staff/Get-AeriesTeacher.ps1
@@ -14,18 +14,26 @@ function Get-AeriesTeacher
         .EXAMPLE
         Get-AeriesTeacher -SchoolCode 994 -TeacherNumber 605
         This will return the Teacher record for School Code 994 and Teacher Number 605 from Aeries
+
+        .EXAMPLE
+        Get-AeriesTeacher -StaffID 994605
+        This will return the Teacher records for Staff ID 994605 from Aeries
     #>
 
     [CmdletBinding()]
     param (
         [ValidateRange(1, [Int16]::MaxValue)]
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory=$false)]
         [Int16]
         $SchoolCode,
         [ValidateRange(0, [Int16]::MaxValue)]
         [Parameter(Mandatory=$false)]
         [Int16]
-        $TeacherNumber = "-1"
+        $TeacherNumber = -1,
+        [ValidateRange(1, [Int]::MaxValue)]
+        [Parameter(Mandatory=$false)]
+        [int]
+        $StaffID
     )
 
     Begin {
@@ -33,10 +41,19 @@ function Get-AeriesTeacher
 
         $Method = "Get"
         $SuccessStatusCode = 200
-        $Endpoint = "v5/schools/$SchoolCode/teachers"
+        $Endpoint = "v5"
 
-        if ($TeacherNumber -ge 0) {
-            $Endpoint += "/$TeacherNumber"
+        if ($SchoolCode -gt 0) {
+            $Endpoint += "/schools/$SchoolCode/teachers"
+            if ($TeacherNumber -ge 0) {
+                $Endpoint += "/$TeacherNumber"
+            }
+        }
+        elseif ($StaffID -gt 0) {
+            $Endpoint += "/staff/$StaffID/teachers"
+        }
+        else {
+            Throw "SchoolCode or StaffID are required"
         }
     }
 
