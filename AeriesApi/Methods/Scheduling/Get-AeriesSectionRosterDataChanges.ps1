10fad25d3785fc6166931286a5e6ef3dd89838eb
Brandon Usher <brandonu@aeries.com>
2021-04-20 15:46:00 -0700

    Implement Section Roster Data Changes GET endpoint

---
+++ AeriesApi/Methods/Scheduling/Get-AeriesSectionRosterDataChanges.ps1
@@ -0,0 +1,39 @@
+function Get-AeriesSectionRosterDataChanges
+{
+    <#  
+        .SYNOPSIS
+        Get Section Roster Data Changes from Aeries
+        
+        .DESCRIPTION
+        The Get-AeriesSectionRosterDataChanges cmdlet is used to retrieve all Section Roster Data Changes from Aeries since a given date
+
+        .EXAMPLE
+        Get-AeriesSectionRosterDataChanges -DateTime (Get-Date "2021-04-09 9:00AM")
+        This will return the requested Section Roster Data Changes from Aeries
+    #>
+
+    [CmdletBinding()]
+    param (
+        # Date Object to use for filters
+        [Parameter(Mandatory=$true)]
+        [datetime]
+        $DateTime
+    )
+
+    Begin {
+        Write-Verbose -Message "Begin running $($MyInvocation.MyCommand)"
+
+        $Method = "Get"
+        $SuccessStatusCode = 200
+        $Endpoint = "v5/SectionRosterDataChanges/" + $DateTime.ToString("yyyy/MM/dd/HH/mm")
+    }
+
+    Process {
+        $Result = (Invoke-AeriesApiCall -Method $Method -Endpoint $Endpoint -SuccessStatusCode $SuccessStatusCode)
+    }
+
+    End {
+        Write-Verbose -Message "Finish running $($MyInvocation.MyCommand)"
+        return $Result
+    }
+}
