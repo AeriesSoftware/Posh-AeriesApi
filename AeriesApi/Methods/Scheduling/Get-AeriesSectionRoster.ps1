1f48fd1072428facacc45c6cbbc1771595579d1e
Brandon Usher <brandonu@aeries.com>
2021-04-20 15:43:46 -0700

    Implement Section Class Roster GET endpoint

---
+++ AeriesApi/Methods/Scheduling/Get-AeriesSectionRoster.ps1
@@ -0,0 +1,43 @@
+function Get-AeriesSectionRoster
+{
+    <#  
+        .SYNOPSIS
+        Get Section Class Roster from Aeries
+        
+        .DESCRIPTION
+        The Get-AeriesSectionRoster cmdlet is used to retrieve a filtered set or all Section Class Roster from Aeries
+
+        .EXAMPLE
+        Get-AeriesSectionRoster -SchoolCode 994 -SectionNumber 43
+        This will return the Section Class Roster for Section 43 from School Code 994 from Aeries
+    #>
+
+    [CmdletBinding()]
+    param (
+        [ValidateRange(1, [Int16]::MaxValue)]
+        [Parameter(Mandatory=$true)]
+        [Int16]
+        $SchoolCode,
+        [ValidateRange(1, [Int16]::MaxValue)]
+        [Parameter(Mandatory=$true)]
+        [Int16]
+        $SectionNumber
+    )
+
+    Begin {
+        Write-Verbose -Message "Begin running $($MyInvocation.MyCommand)"
+
+        $Method = "Get"
+        $SuccessStatusCode = 200
+        $Endpoint = "v5/schools/$SchoolCode/sections/$SectionNumber/students"
+    }
+
+    Process {
+        $Result = (Invoke-AeriesApiCall -Method $Method -Endpoint $Endpoint -SuccessStatusCode $SuccessStatusCode)
+    }
+
+    End {
+        Write-Verbose -Message "Finish running $($MyInvocation.MyCommand)"
+        return $Result
+    }
+}
