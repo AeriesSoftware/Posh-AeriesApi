05321e5af3b2cfd9779b1dfc30626531cc4182b8
Brandon Usher <brandonu@aeries.com>
2021-10-02 14:23:45 -0700

    Implement Remove-AeriesContact

---
+++ AeriesApi/Public/Remove-AeriesContact.ps1
@@ -0,0 +1,44 @@
+function Remove-AeriesContact
+{
+    <#
+        .SYNOPSIS
+        Removes a Contact in Aeries
+
+        .DESCRIPTION
+        The Remove-AeriesContact cmdlet is used to remove a Contact in Aeries for a given StudentID and Sequence Number
+
+        .EXAMPLE
+        Remove-AeriesContact -StudentID 99400001 -Sequence 1
+        This will remove a Contact in Aeries for StudentID 99400001 with a Sequence Number of 1
+    #>
+
+    [CmdletBinding()]
+    param (
+        [ValidateRange(1, [Int]::MaxValue)]
+        [Parameter(Mandatory=$true)]
+        [int]
+        $StudentID,
+
+        [ValidateRange(1, [Int16]::MaxValue)]
+        [Parameter(Mandatory=$true)]
+        [Int16]
+        $Sequence
+    )
+
+    Begin {
+        Write-Verbose -Message "Begin running $($MyInvocation.MyCommand)"
+
+        $Method = "Delete"
+        $SuccessStatusCode = 200
+        $Endpoint = "v5/DeleteContact/$StudentID/$Sequence"
+    }
+
+    Process {
+        $Result = (Invoke-AeriesApiCall -Method $Method -Endpoint $Endpoint -SuccessStatusCode $SuccessStatusCode)
+    }
+
+    End {
+        Write-Verbose -Message "Finish running $($MyInvocation.MyCommand)"
+        return $Result
+    }
+}
