0775ee55f88d274539484744d82e76b3fc86abe1
Brandon Usher <brandonu@aeries.com>
2021-10-02 14:35:44 -0700

    Add support for ShouldProcess to allow -WhatIf

--- AeriesApi/Public/Remove-AeriesContact.ps1
+++ AeriesApi/Public/Remove-AeriesContact.ps1
@@ -12,7 +12,7 @@ function Remove-AeriesContact
         This will remove a Contact in Aeries for StudentID 99400001 with a Sequence Number of 1
     #>
 
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess)]
     param (
         [ValidateRange(1, [Int]::MaxValue)]
         [Parameter(Mandatory=$true)]
@@ -34,7 +34,9 @@ function Remove-AeriesContact
     }
 
     Process {
-        $Result = (Invoke-AeriesApiCall -Method $Method -Endpoint $Endpoint -SuccessStatusCode $SuccessStatusCode)
+        if($PSCmdlet.ShouldProcess("Contact Sequence $Sequence for Student ID $StudentID")) {
+            $Result = (Invoke-AeriesApiCall -Method $Method -Endpoint $Endpoint -SuccessStatusCode $SuccessStatusCode)
+        }
     }
 
     End {
